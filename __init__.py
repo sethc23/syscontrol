bc7440e5396083b88b2acdbb420123c8f7eafc36
sethc23 <sethc23@users.noreply.github.com>
2016-04-26 13:06:49 -0400

    moved recursive importing of related submodules into definition and added conditional

--- __init__.py
+++ __init__.py
@@ -7,20 +7,27 @@ py_path.append(                                 os.environ['HOME_ENV'] + '/.scri
 from system_settings                            import *
 from sysparse                                   import *
 
-# Import all from all
-from importlib import import_module
-import inspect
-_from_file = inspect.stack()[-1][1]
-_from_file = _from_file[_from_file.rfind('/')+1:]
-exclude_py = ['__init__.py',_from_file,'sys_control.py',]
-dir_list = sorted([it for it in os.listdir(os.getcwd()) 
-                    if it[-3:]=='.py' and not exclude_py.count(it)])
+def ImportAllSysModules():
+    # Import all from all
+    from importlib import import_module
+    import inspect
+    _from_file = inspect.stack()[-1][1]
+    _from_file = _from_file[_from_file.rfind('/')+1:]
+    exclude_py = ['__init__.py',_from_file,'sys_control.py',]
+    dir_list = sorted([it for it in os.listdir(os.getcwd()) 
+                        if it[-3:]=='.py' and not exclude_py.count(it)])
 
-for _file in dir_list:
-    _module = _file.rsplit('.')[0]
-    imported_mod = import_module(_module)
-    exec '%s = getattr(imported_mod,_module)' % _module
+    for _file in dir_list:
+        print _file
+        print os.getcwd()
+        _module = _file.rsplit('.')[0]
+        imported_mod = import_module(_module)
+        exec '%s = getattr(imported_mod,_module)' % _module
 
+if os.path.abspath(os.getcwd())!=os.path.join(os.environ['HOME'],'.scripts'):
+    pass
+else:
+    ImportAllSysModules()
 
 try:
     from ipdb import set_trace as i_trace   # i_trace()
